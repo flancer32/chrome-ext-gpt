4482a1a41be4354b1f35fed125071ef5bc97a5d9
Alex Gusev <alex@flancer64.com>
2025-08-28 07:37:10 +0300

    Add the openai package to the project

--- src/js/container.js
+++ src/js/container.js
@@ -1,9 +1,10 @@
+// use relative path to allow running in browser and in Node.js
 import Container from "../../node_modules/@teqfw/di/src/Container.js";
 
 // In Chrome extension context (ESM)
 const url = new URL(import.meta.url);
-// url.href -> full URL to this module
-const currentDir = url.href.substring(0, url.href.lastIndexOf("/") + 1);
+const appDir = new URL("./di", url).href;
+const openAiDir = new URL("../../node_modules/openai", url).href;
 
 /** @type {TeqFw_Di_Container} */
 const container = new Container();
@@ -11,6 +12,7 @@ const container = new Container();
 const resolver = container.getResolver();
 
 // Define the namespace root for dependencies, allowing the container to resolve identifiers like 'App_*'
-resolver.addNamespaceRoot("GptExt_", currentDir + "di");
+resolver.addNamespaceRoot("GptExt_", appDir);
+resolver.addNamespaceRoot("openai_", openAiDir);
 
 export default container;
